b42a83225da9f494fec258b0435537a6b1fc50f5
Jean-Philippe Metivier <jean-philippe.metivier@unicaen.fr>
2018-11-19 10:02:44 +0100

    privilege pour les applications, les activites et les affectations

--- module/Application/src/Application/Provider/Privilege/ActivitePrivileges.php
+++ module/Application/src/Application/Provider/Privilege/ActivitePrivileges.php
@@ -4,6 +4,8 @@ namespace Application\Provider\Privilege;
 
 class ActivitePrivileges extends \UnicaenAuth\Provider\Privilege\Privileges
 {
+    const AJOUTER           = 'activite-ajouter-activite';
     const AFFICHER          = 'activite-afficher-activite';
     const EDITER            = 'activite-editer-activite';
+    const EFFACER           = 'activite-effacer-activite';
 }
