6b872866672560da285621284014b9500bf4e7a5
Jean-Philippe Metivier <jean-philippe.metivier@unicaen.fr>
2018-12-17 13:33:46 +0100

    Association du poste à la fiche metier

---
+++ module/Application/src/Application/Form/FicheMetier/AssocierPosteFormFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Application\Form\FicheMetier;
+
+use Doctrine\ORM\EntityManager;
+use Zend\Form\FormElementManager;
+
+class AssocierPosteFormFactory {
+
+    public function __invoke(FormElementManager $manager)
+    {
+        /** @var AssocierPosteHydrator $hydrator */
+        $hydrator = $manager->getServiceLocator()->get('HydratorManager')->get(AssocierPosteHydrator::class);
+
+        /** @var EntityManager $entityManager **/
+        $entityManager = $manager->getServiceLocator()->get('doctrine.entitymanager.orm_default');
+
+        /** @var AssocierPosteForm $form */
+        $form = new AssocierPosteForm();
+        $form->setEntityManager($entityManager);
+
+        $form->setHydrator($hydrator);
+        $form->init();
+
+
+        return $form;
+    }
+
+}
