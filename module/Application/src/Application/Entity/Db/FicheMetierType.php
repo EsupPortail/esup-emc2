9feaebd814dd459d3c49c0b1030a6f30cf55efed
Jean-Philippe Metivier <jean-philippe.metivier@unicaen.fr>
2018-12-17 17:47:13 +0100

    Version naze du tableau de la fiche de metier type

--- module/Application/src/Application/Entity/Db/FicheMetierType.php
+++ module/Application/src/Application/Entity/Db/FicheMetierType.php
@@ -13,6 +13,19 @@ class FicheMetierType {
     /** @var string */
     private $missionsPrincipales;
 
+    /** @var string */
+    private $connaissances;
+    /** @var string */
+    private $connaissancesFormation;
+    /** @var string */
+    private $competencesOperationnelles;
+    /** @var string */
+    private $competencesOperationnellesFormation;
+    /** @var string */
+    private $competencesComportementales;
+    /** @var string */
+    private $competencesComportementalesFormation;
+
     /**
      * @return int
      */
@@ -56,4 +69,115 @@ class FicheMetierType {
         $this->missionsPrincipales = $missionsPrincipales;
         return $this;
     }
+
+    /**
+     * @return string
+     */
+    public function getConnaissances()
+    {
+        return $this->connaissances;
+    }
+
+    /**
+     * @param string $connaissances
+     * @return FicheMetierType
+     */
+    public function setConnaissances($connaissances)
+    {
+        $this->connaissances = $connaissances;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getConnaissancesFormation()
+    {
+        return $this->connaissancesFormation;
+    }
+
+    /**
+     * @param string $connaissancesFormation
+     * @return FicheMetierType
+     */
+    public function setConnaissancesFormation($connaissancesFormation)
+    {
+        $this->connaissancesFormation = $connaissancesFormation;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCompetencesOperationnelles()
+    {
+        return $this->competencesOperationnelles;
+    }
+
+    /**
+     * @param string $competencesOperationnelles
+     * @return FicheMetierType
+     */
+    public function setCompetencesOperationnelles($competencesOperationnelles)
+    {
+        $this->competencesOperationnelles = $competencesOperationnelles;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCompetencesOperationnellesFormation()
+    {
+        return $this->competencesOperationnellesFormation;
+    }
+
+    /**
+     * @param string $competencesOperationnellesFormation
+     * @return FicheMetierType
+     */
+    public function setCompetencesOperationnellesFormation($competencesOperationnellesFormation)
+    {
+        $this->competencesOperationnellesFormation = $competencesOperationnellesFormation;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCompetencesComportementales()
+    {
+        return $this->competencesComportementales;
+    }
+
+    /**
+     * @param string $competencesComportementales
+     * @return FicheMetierType
+     */
+    public function setCompetencesComportementales($competencesComportementales)
+    {
+        $this->competencesComportementales = $competencesComportementales;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCompetencesComportementalesFormation()
+    {
+        return $this->competencesComportementalesFormation;
+    }
+
+    /**
+     * @param string $competencesComportementalesFormation
+     * @return FicheMetierType
+     */
+    public function setCompetencesComportementalesFormation($competencesComportementalesFormation)
+    {
+        $this->competencesComportementalesFormation = $competencesComportementalesFormation;
+        return $this;
+    }
+
+
+
 }
---
+++ module/Application/src/Application/Form/FicheMetierType/FormationBaseFormFactory.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Application\Form\FicheMetierType;
+
+use Zend\Form\FormElementManager;
+
+class FormationBaseFormFactory{
+
+    public function __invoke(FormElementManager $manager)
+    {
+        /** @var FormationBaseHydrator $hydrator */
+        $hydrator = $manager->getServiceLocator()->get('HydratorManager')->get(FormationBaseHydrator::class);
+
+        $form = new FormationBaseForm();
+        $form->init();
+        $form->setHydrator($hydrator);
+
+        return $form;
+    }
+}
