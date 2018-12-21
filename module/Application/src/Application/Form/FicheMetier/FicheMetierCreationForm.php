cd3c284f788612199a28a16f442bc0dc162cbba8
Jean-Philippe Metivier <jean-philippe.metivier@unicaen.fr>
2018-12-21 17:04:45 +0100

    Retrait de l'affectation à la création d'une fiche metier

--- module/Application/src/Application/Form/FicheMetier/FicheMetierCreationForm.php
+++ module/Application/src/Application/Form/FicheMetier/FicheMetierCreationForm.php
@@ -2,15 +2,11 @@
 
 namespace Application\Form\FicheMetier;
 
-use Application\Entity\Db\Affectation;
-use DoctrineModule\Form\Element\ObjectSelect;
-use UnicaenApp\Service\EntityManagerAwareTrait;
 use Zend\Form\Element\Button;
 use Zend\Form\Element\Text;
 use Zend\Form\Form;
 
 class FicheMetierCreationForm extends Form {
-    use EntityManagerAwareTrait;
 
     public function init()
     {
@@ -23,29 +19,6 @@ class FicheMetierCreationForm extends Form {
             ],
             'attributes' => [
                 'id' => 'libelle',
-            ],
-        ]);
-        //Selection obligatoire AFFECTATION
-        $this->add([
-            'type' => ObjectSelect::class,
-            'name' => 'affectation',
-            'options' => [
-                'label' => "Service/composante/direction d'affectation :",
-                'empty_option' => "Sélectionner une affectation",
-                'object_manager' => $this->getEntityManager(),
-                'target_class' => Affectation::class,
-                'property' => 'libelle',
-                'find_method' => [
-                    'name' => 'findBy',
-                    'params' => [
-                        'criteria' => [],
-                        'orderBy' => ['libelle' => 'ASC'],
-                    ],
-                ],
-                'disable_inarray_validator' => true,
-            ],
-            'attributes' => [
-                'id' => 'affectation',
             ],
         ]);
 
