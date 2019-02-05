f53bb189e863d7115a1c21ed5fe7a46f565e6928
Jean-Philippe Metivier <jean-philippe.metivier@unicaen.fr>
2019-02-05 11:18:09 +0100

    Trait pour ActiviteForm

--- module/Application/src/Application/Controller/FicheMetier/FicheMetierTypeControllerFactory.php
+++ module/Application/src/Application/Controller/FicheMetier/FicheMetierTypeControllerFactory.php
@@ -2,25 +2,33 @@
 
 namespace Application\Controller\FicheMetier;
 
+use Application\Form\Activite\ActiviteForm;
 use Application\Service\Activite\ActiviteService;
 use Application\Service\FicheMetier\FicheMetierService;
 use Zend\Mvc\Controller\ControllerManager;
 
 class FicheMetierTypeControllerFactory {
 
-    public function __invoke(ControllerManager $controllerManager)
+    public function __invoke(ControllerManager $manager)
     {
         /**
          * @var ActiviteService $activiteService
          * @var FicheMetierService $ficheMetierService
          */
-        $activiteService = $controllerManager->getServiceLocator()->get(ActiviteService::class);
-        $ficheMetierService = $controllerManager->getServiceLocator()->get(FicheMetierService::class);
+        $activiteService = $manager->getServiceLocator()->get(ActiviteService::class);
+        $ficheMetierService = $manager->getServiceLocator()->get(FicheMetierService::class);
+
+        /**
+         * @var ActiviteForm $activiteForm
+         */
+        $activiteForm = $manager->getServiceLocator()->get('FormElementManager')->get(ActiviteForm::class);
+
 
         /** @var FicheMetierControllerFactory.php $controller */
         $controller = new FicheMetierTypeController();
         $controller->setActiviteService($activiteService);
         $controller->setFicheMetierService($ficheMetierService);
+        $controller->setActiviteForm($activiteForm);
         return $controller;
     }
 
