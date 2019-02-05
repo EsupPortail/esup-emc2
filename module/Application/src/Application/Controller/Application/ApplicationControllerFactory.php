772480263a4536b8dc1f9cbe3c223e635a13040d
Jean-Philippe Metivier <jean-philippe.metivier@unicaen.fr>
2019-02-05 11:49:38 +0100

    Trait pour ApplicationForm

--- module/Application/src/Application/Controller/Application/ApplicationControllerFactory.php
+++ module/Application/src/Application/Controller/Application/ApplicationControllerFactory.php
@@ -2,21 +2,28 @@
 
 namespace Application\Controller\Application;
 
+use Application\Form\Application\ApplicationForm;
 use Application\Service\Application\ApplicationService;
 use Zend\Mvc\Controller\ControllerManager;
 
 class ApplicationControllerFactory {
 
-    public function __invoke(ControllerManager $controllerManager)
+    public function __invoke(ControllerManager $manager)
     {
         /**
          * @var ApplicationService $applicationService
          */
-        $applicationService = $controllerManager->getServiceLocator()->get(ApplicationService::class);
+        $applicationService = $manager->getServiceLocator()->get(ApplicationService::class);
+
+        /**
+         * @var ApplicationForm $applicationForm
+         */
+        $applicationForm = $manager->getServiceLocator()->get('FormElementManager')->get(ApplicationForm::class);
 
         /** @var ApplicationController $controller */
         $controller = new ApplicationController();
         $controller->setApplicationService($applicationService);
+        $controller->setApplicationForm($applicationForm);
         return $controller;
     }
 }
