5050a28a585ccc12a3cb0710b6dee0e311b9eec1
Jean-Philippe Metivier <jean-philippe.metivier@unicaen.fr>
2019-02-06 09:50:03 +0100

    Traits associés aux formulaires des fiches metiers

---
+++ module/Application/src/Application/Form/FicheMetier/AssocierAgentFormAwareTrait.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Application\Form\FicheMetier;
+
+trait AssocierAgentFormAwareTrait {
+
+    /** @var AssocierAgentForm $associerAgentForm */
+    private $associerAgentForm;
+
+    /**
+     * @return AssocierAgentForm
+     */
+    public function getAssocierAgentForm()
+    {
+        return $this->associerAgentForm;
+    }
+
+    /**
+     * @param AssocierAgentForm $associerAgentForm
+     * @return AssocierAgentForm
+     */
+    public function setAssocierAgentForm($associerAgentForm)
+    {
+        $this->associerAgentForm = $associerAgentForm;
+        return $this->associerAgentForm;
+    }
+}
