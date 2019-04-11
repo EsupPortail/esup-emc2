f71ed5748bb96366989d17f68394c2de43a34019
Jean-Philippe Metivier <jean-philippe.metivier@unicaen.fr>
2019-04-11 14:18:50 +0200

    cache et env

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -1,6 +1,36 @@
 <?php
-return array(
-    'translator' => array(
+
+$env = getenv('APPLICATION_ENV') ?: 'production';
+
+return [
+    'translator' => [
         'locale' => 'fr_FR',
-    ),
-);
+    ],
+
+    'module_listener_options' => [
+        'config_cache_enabled'     => ($env === 'production'),
+        'config_cache_key'         => 'app_config',
+        'module_map_cache_enabled' => ($env === 'production'),
+        'module_map_cache_key'     => 'module_map',
+        'cache_dir'                => 'data/config/',
+        'check_dependencies'       => ($env !== 'production'),
+    ],
+    'doctrine' => [
+        'configuration' => [
+            'orm_default' => [
+                'metadata_cache'   => ($env === 'production' ? 'memcached' : 'array'),
+                'query_cache'      => ($env === 'production' ? 'memcached' : 'array'),
+                'result_cache'     => ($env === 'production' ? 'memcached' : 'array'),
+                'hydration_cache'  => ($env === 'production' ? 'memcached' : 'array'),
+                'generate_proxies' => ($env !== 'production'),
+            ],
+            'orm_octopus' => [
+                'metadata_cache'   => ($env === 'production' ? 'memcached' : 'array'),
+                'query_cache'      => ($env === 'production' ? 'memcached' : 'array'),
+                'result_cache'     => ($env === 'production' ? 'memcached' : 'array'),
+                'hydration_cache'  => ($env === 'production' ? 'memcached' : 'array'),
+                'generate_proxies' => ($env !== 'production'),
+            ],
+        ],
+    ],
+];
