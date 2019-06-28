724207acd3ea3c6d0f3a4796fa358f2680fccd49
Jean-Philippe Metivier <jean-philippe.metivier@unicaen.fr>
2019-06-28 15:50:22 +0200

    création d'input-helper individuel et en phtml

--- module/Autoform/src/Autoform/View/Helper/ChampAsResultHelper.php
+++ module/Autoform/src/Autoform/View/Helper/ChampAsResultHelper.php
@@ -26,61 +26,24 @@ class ChampAsResultHelper extends AbstractHelper
                 break;
             case Champ::TYPE_TEXT :
                 $texte .= $champ->getLibelle(). ' : ';
-
-                if ($data === null) {
-                    $length = rand(8, 20);
-                    for ($i = 0; $i < $length; $i++) {
-                        $value = rand(0, 25) + 97;
-                        $texte .= chr($value);
-                        $space = rand(0, 8);
-                        if ($space === 0) $texte .= ' ';
-                    }
-                }
                 if ($data !== '') {
                     $texte .= $data;
                 }
                 break;
             case Champ::TYPE_TEXTAREA :
                 $texte .= $champ->getLibelle(). ' : ';
-
-                if ($data === null) {
-                    $length = rand(50, 200);
-                    for ($i = 0; $i < $length; $i++) {
-                        $value = rand(0, 25) + 97;
-                        $texte .= chr($value);
-                        $space = rand(0, 8);
-                        if ($space === 0) $texte .= ' ';
-                    }
-                }
                 if ($data !== '') {
                     $texte .= $data;
                 }
                 break;
             case Champ::TYPE_SELECT :
                 $texte .= $champ->getLibelle(). ' : ';
-
-                if ($data === null) {
-                    $options = explode(';', $champ->getOptions());
-                    $position = rand(0, count($options) - 1);
-                    $texte .= $options[$position];
-                }
                 if ($data !== 'null') {
                     $texte .= $data;
                 }
                 break;
             case Champ::TYPE_PERIODE :
                 $texte .= $champ->getLibelle(). ' : ';
-
-                if ($data === null) {
-                    $periodes = ['Année entière', 'Premier semestre', 'Second semestre', 'Baslisée'];
-                    $periode = rand(0, 3);
-
-                    if ($periode !== 3) {
-                        $texte .= $periodes[$periode];
-                    } else {
-                        $texte .= 'du ' . '01/01/2111' . ' au ' . '02/02/2222';
-                    }
-                }
                 if ($data !== 'null') {
                     $texte .= $data;
                 }
