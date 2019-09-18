59d7f16a724833ce312c6fe34cff8d800a77c982
Jean-Philippe Metivier <jean-philippe.metivier@unicaen.fr>
2019-09-18 11:12:00 +0200

    Correction du problème de remonté des gestionnaires de structures

--- module/Application/src/Application/Controller/StructureController.php
+++ module/Application/src/Application/Controller/StructureController.php
@@ -36,6 +36,7 @@ class StructureController extends AbstractActionController {
     public function afficherAction()
     {
         $structure = $this->getStructureService()->getRequestedStructure($this, 'structure');
+        $structure = $this->getStructureService()->getStructure(417);
 
         return new ViewModel([
             'structure' => $structure,
