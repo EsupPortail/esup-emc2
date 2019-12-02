8465f12d8aa9eb4f627bfe3e2c37931203411dcc
Jean-Philippe Metivier <jean-philippe.metivier@unicaen.fr>
2019-12-02 15:57:37 +0100

    Début de rendu graphique des structures

--- module/Application/src/Application/Controller/StructureController.php
+++ module/Application/src/Application/Controller/StructureController.php
@@ -2,8 +2,12 @@
 
 namespace Application\Controller;
 
+use Application\Entity\Db\Structure;
 use Application\Form\Structure\StructureFormAwareTrait;
+use Application\Service\Agent\AgentServiceAwareTrait;
 use Application\Service\Structure\StructureServiceAwareTrait;
+use Fhaculty\Graph\Graph;
+use Graphp\GraphViz\GraphViz;
 use UnicaenApp\Exception\RuntimeException;
 use Utilisateur\Entity\Db\Role;
 use Utilisateur\Entity\Db\User;
@@ -17,6 +21,7 @@ class StructureController extends AbstractActionController {
     use RoleServiceAwareTrait;
     use StructureServiceAwareTrait;
     use UserServiceAwareTrait;
+    use AgentServiceAwareTrait;
 
     use StructureFormAwareTrait;
 
@@ -132,4 +137,114 @@ class StructureController extends AbstractActionController {
         return $vm;
     }
 
+    public function grapheAction() {
+        $graph = new Graph();
+        $structureMere = $this->getStructureService()->getRequestedStructure($this);
+
+        $structures = [];
+        if ($structureMere === null) {
+            $structures = $this->getStructureService()->getStructures(true);
+        } else {
+            $file = [];
+            $file[] = $structureMere;
+            while (!empty($file)) {
+                /** @var Structure $structure */
+                $structure = array_shift($file);
+
+                $structures[] = $structure;
+                $enfants = $structure->getEnfants()->toArray();
+                /** @var Structure $enfant */
+                foreach ($enfants as $enfant) {
+                    if ($enfant->getHisto() === 'O' ) {
+                        $file[] = $enfant;
+                    }
+                }
+            }
+        }
+        $array = [];
+        foreach ($structures as $structure) {
+            $agentsTexte = "";
+            $agents = $this->getAgentService()->getAgentsByStructure($structure);
+            foreach ($agents as $agent) {
+                $agentsTexte .= "- " . $agent->getDenomination() . "\n";
+            }
+            $vertex = $graph->createVertex($structure->getId() . " - " . $structure->getLibelleCourt() . "\n" . $agentsTexte);
+            switch ($structure->getType()) {
+                case "Établissement" :
+                    $vertex->setAttribute('graphviz.shape', 'box3d');
+                    break;
+                case "Composante" :
+                    $vertex->setAttribute('graphviz.shape', 'rect');
+                    $vertex->setAttribute('graphviz.color', 'blue');
+                    $vertex->setAttribute('graphviz.fontcolor', 'blue');
+                    break;
+                case "Bibliothèque" :
+                    $vertex->setAttribute('graphviz.shape', 'egg');
+                    $vertex->setAttribute('graphviz.color', 'Hotpink');
+                    $vertex->setAttribute('graphviz.fontcolor', 'Hotpink');
+                    break;
+                case "Antenne" :
+                case "Département" :
+                    $vertex->setAttribute('graphviz.shape', 'rect');
+                    $vertex->setAttribute('graphviz.color', 'blue');
+                    $vertex->setAttribute('graphviz.style', 'dashed');
+                    $vertex->setAttribute('graphviz.fontcolor', 'blue');
+                    break;
+                case "Structure de recherche" :
+                    $vertex->setAttribute('graphviz.shape', 'rect');
+                    $vertex->setAttribute('graphviz.style', 'rounded');
+                    $vertex->setAttribute('graphviz.color', 'green');
+                    $vertex->setAttribute('graphviz.fontcolor', 'green');
+                    break;
+                case "Service central" :
+                    $vertex->setAttribute('graphviz.shape', 'hexagon');
+                    $vertex->setAttribute('graphviz.color', 'red');
+                    $vertex->setAttribute('graphviz.fontcolor', 'red');
+                    break;
+                case "Service commun" :
+                    $vertex->setAttribute('graphviz.shape', 'hexagon');
+                    $vertex->setAttribute('graphviz.color', 'darkorange');
+                    $vertex->setAttribute('graphviz.fontcolor', 'darkorange');
+                    break;
+                case "Sous-structure administrative" :
+                    $vertex->setAttribute('graphviz.shape', 'hexagon');
+                    $vertex->setAttribute('graphviz.style', 'dashed');
+                    $typeParent = null;
+                    $parent = $structure->getParent();
+                    while ($typeParent === null AND $parent !== null) {
+                        if ($parent->getType() !== "Sous-structure administrative") $typeParent = $parent->getType();
+                        $parent = $parent->getParent();
+                    }
+                    if ($typeParent === "Service central") {
+                        $vertex->setAttribute('graphviz.color', 'red');
+                        $vertex->setAttribute('graphviz.fontcolor', 'red');
+                    }
+                    if ($typeParent === "Service commun") {
+                        $vertex->setAttribute('graphviz.color', 'darkorange');
+                        $vertex->setAttribute('graphviz.fontcolor', 'darkorange');
+                    }
+                    if ($typeParent === "Composante" OR $typeParent === "Antenne" OR $typeParent === "Département" ) {
+                        $vertex->setAttribute('graphviz.color', 'blue');
+                        $vertex->setAttribute('graphviz.fontcolor', 'blue');
+                    }
+                    break;
+            }
+            $array[$structure->getId()] = $vertex;
+
+        }
+        foreach ($structures as $structure) {
+            $son = $array[$structure->getId()];
+            $father = null;
+            if ($structure->getParent()) $father = $array[$structure->getParent()->getId()];
+
+            if ($father && $son) $father->createEdgeTo($son);
+        }
+
+        $viz = new GraphViz();
+//        $img = $viz->createImageData($graph);
+        $img = $viz->createImageHtml($graph);
+        return new ViewModel([
+           'img' => $img,
+        ]);
+    }
 }
