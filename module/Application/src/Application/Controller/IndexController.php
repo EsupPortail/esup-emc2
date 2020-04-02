65171fd559de568ba8d8f0361cd9b5d1393841f0
Jean-Philippe Metivier <jean-philippe.metivier@unicaen.fr>
2020-04-02 11:41:59 +0200

    Amélioration de l'indexController

--- module/Application/src/Application/Controller/IndexController.php
+++ module/Application/src/Application/Controller/IndexController.php
@@ -1,21 +1,14 @@
 <?php
-/**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
- * @copyright Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
- */
 
 namespace Application\Controller;
 
 use Application\Constant\RoleConstant;
-use Application\Entity\Db\AgentMissionSpecifique;
-use Application\Form\AgentMissionSpecifique\AgentMissionSpecifiqueForm;
+use Application\Entity\Db\Agent;
 use Application\Service\Agent\AgentServiceAwareTrait;
 use Application\Service\Validation\ValidationDemandeServiceAwareTrait;
 use UnicaenAuthentification\Service\Traits\UserContextServiceAwareTrait;
 use UnicaenUtilisateur\Entity\Db\Role;
+use UnicaenUtilisateur\Entity\Db\User;
 use UnicaenUtilisateur\Service\Role\RoleServiceAwareTrait;
 use UnicaenUtilisateur\Service\User\UserServiceAwareTrait;
 use Zend\Mvc\Controller\AbstractActionController;
@@ -32,70 +25,50 @@ class IndexController extends AbstractActionController
 
     public function indexAction()
     {
-        $identity = null;
+        /** @var User $connectedUser */
+        $connectedUser = $this->getUserService()->getConnectedUser();
+        /** @var Agent $agent */
         $agent = null;
 
 
-        if ($this->getUserService()->getServiceUserContext()->getLdapUser()) {
-            $identity = $this->getUserService()->getConnectedUser();
-            $role = $this->getUserService()->getConnectedRole();
+        if ($this->getServiceUserContext()->getLdapUser()) {
             $supannId = ((int) $this->getServiceUserContext()->getLdapUser()->getSupannEmpId());
             $agent = $this->getAgentService()->getAgentBySupannId($supannId);
 
-            if ($identity !== null && $agent !== null && $agent->getUtilisateur() === null) {
-                $agent->setUtilisateur($identity);
+            if ($connectedUser !== null && $agent !== null && $agent->getUtilisateur() === null) {
+                $agent->setUtilisateur($connectedUser);
                 $this->getAgentService()->update($agent);
                 $personnel = $this->getRoleService()->getRoleByCode(RoleConstant::PERSONNEL);
-                $hasAgent = $identity->hasRole($personnel);
-                if (! $hasAgent) $this->getUserService()->addRole($identity, $personnel);
+                $hasAgent = $connectedUser->hasRole($personnel);
+                if (! $hasAgent) $this->getUserService()->addRole($connectedUser, $personnel);
                 return $this->redirect()->toRoute('agent/afficher', ['id' => $agent->getId()], [], true);
             }
         }
 
         /** @var Role $connectedRole */
         $connectedRole = $this->getUserService()->getConnectedRole();
-        $connectedUser = $this->getUserService()->getConnectedUser();
-
 
         if ($connectedRole) {
             switch ($connectedRole->getRoleId()) {
                 case RoleConstant::PERSONNEL :
-//                    $supannId = ((int) $this->getServiceUserContext()->getLdapUser()->getSupannEmpId());
                     $agent = $this->getAgentService()->getAgentByUser($connectedUser);
                     return $this->redirect()->toRoute('agent/afficher', ['id' => $agent->getId()], [], true);
                     break;
                 case RoleConstant::VALIDATEUR :
                     return $this->redirect()->toRoute('index-validateur', [], [], true);
                     break;
-                default :
-                    //var_dump($connectedRole);
-                    break;
             }
         }
 
         return new ViewModel([
-            'user' => $identity,
+            'user' => $connectedUser,
             'agent' => $agent,
         ]);
     }
 
-    public function indexPersonnelAction() {
-
-        $user = $this->getUserService()->getConnectedUser();
-        $agent = $this->getAgentService()->getAgentByUser($user);
-        $missions = $agent->getMissionsSpecifiques();
-        usort($missions, function (AgentMissionSpecifique $a, AgentMissionSpecifique $b) { return $a->getDateDebut() > $b->getDateDebut();});
-
-        return new ViewModel([
-            'user' => $user,
-            'agent' => $agent,
-            'fiche' => (!empty($agent->getFiches()))?current($agent->getFiches()):null,
-            'missions' => $missions,
-        ]);
-    }
-
-    public function indexValidateurAction() {
-
+    public function indexValidateurAction()
+    {
+        /** @var User $user */
         $user = $this->getUserService()->getConnectedUser();
         $demandes = $this->getValidationDemandeService()->getValidationsDemandesByValidateur($user);
 
@@ -106,7 +79,4 @@ class IndexController extends AbstractActionController
         ]);
     }
 
-    public function administrationAction() {
-        return new ViewModel();
-    }
 }
