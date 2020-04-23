99164af6b14dc77091fdecd4738510678688ad6b
Jean-Philippe Metivier <jean-philippe.metivier@unicaen.fr>
2020-04-23 11:33:49 +0200

    Usage des dictionnaires applications/formations/compétences pour les graphes

--- module/Application/src/Application/View/Helper/FichePosteGraphViewHelper.php
+++ module/Application/src/Application/View/Helper/FichePosteGraphViewHelper.php
@@ -16,12 +16,13 @@ class FichePosteGraphViewHelper extends AbstractHelper {
 
     /**
      * @param FichePoste $ficheposte
+     * @param array $dictionnaires
      * @param string $mode
      * @param DateTime $date
      * @param array $options
      * @return string|Partial
      */
-    public function __invoke($ficheposte, $mode = 'radar', $date = null, $options = ['debug' => true])
+    public function __invoke($ficheposte, $dictionnaires = [], $mode = 'radar', $date = null, $options = ['debug' => true])
     {
         $options['mode'] = $mode;
         /** @var PhpRenderer $view */
@@ -36,15 +37,25 @@ class FichePosteGraphViewHelper extends AbstractHelper {
 
         $data = [];
 
-        $allApplications = $ficheposte->getApplications($date);
-        $conApplications = $ficheposte->getApplicationsConservees($date);
-        $data['Applications'] = empty($allApplications)?0:(count($conApplications) / count($allApplications)) * 100;
-        $allCompetences =  $ficheposte->getCompetences($date);
-        $conCompetences = $ficheposte->getCompetencesConservees($date);
-        $data['Compétences'] =  empty($allCompetences)?0:(count($conCompetences) / count($allCompetences)) * 100;
-        $allFormations =  $ficheposte->getFormations($date);
-        $conFormations =  $ficheposte->getFormationsConservees($date);
-        $data['Formations'] = empty($allFormations)?0:(count($conFormations) / count($allFormations)) * 100;
+        $data['Applications'] = 0;
+        $data['Compétences'] = 0;
+        $data['Formations'] = 0;
+        if (isset($dictionnaires["applications"]) AND $dictionnaires["applications"] !== null) {
+            $nbApplicationsConservees = 0; $nbApplications = count($dictionnaires["applications"]);
+            foreach ($dictionnaires["applications"] as $item) if ($item["conserve"] === true) $nbApplicationsConservees++;
+            $data['Applications'] = $nbApplicationsConservees / $nbApplications * 100;
+        }
+        if (isset($dictionnaires["competences"]) AND $dictionnaires["competences"] !== null) {
+            $nbCompetencesConservees = 0; $nbCompetences = count($dictionnaires["competences"]);
+            foreach ($dictionnaires["competences"] as $item) if ($item["conserve"] === true) $nbCompetencesConservees++;
+            $data['Compétences'] = $nbCompetencesConservees / $nbCompetences * 100;
+        }
+        if (isset($dictionnaires["formations"]) AND $dictionnaires["formations"] !== null) {
+            $nbFormationsConservees = 0; $nbFormations = count($dictionnaires["formations"]);
+            foreach ($dictionnaires["formations"] as $item) if ($item["conserve"] === true) $nbFormationsConservees++;
+            $data['Formations'] = $nbFormationsConservees / $nbFormations * 100;
+        }
+
         $allActivites =  $ficheposte->getActivites($date);
         $conActivites =  $ficheposte->getActivitesConservees($date);
         $data['Activités'] = empty($allActivites)?0:(count($conActivites) / count($allActivites)) * 100;
