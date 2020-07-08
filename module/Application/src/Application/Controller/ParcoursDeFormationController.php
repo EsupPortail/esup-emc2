c714a139d989d451187c3a38367f9007198724c3
Jean-Philippe Metivier <jean-philippe.metivier@unicaen.fr>
2020-07-08 09:54:28 +0200

    Ajout des actions manquantes pour le parcours de formations

--- module/Application/src/Application/Controller/ParcoursDeFormationController.php
+++ module/Application/src/Application/Controller/ParcoursDeFormationController.php
@@ -92,4 +92,51 @@ class ParcoursDeFormationController extends AbstractActionController
         ]);
         return $vm;
     }
+
+    private function getFragment(ParcoursDeFormation $parcours)
+    {
+        $type = $parcours->getType();
+        if ($type === ParcoursDeFormation::TYPE_METIER) return "metier";
+        if ($type === ParcoursDeFormation::TYPE_CATEGORIE) return "categorie";
+        return "";
+    }
+
+    public function historiserAction()
+    {
+        $parcours = $this->getParcoursDeFormationService()->getRequestedParcoursDeFormation($this);
+        $this->getParcoursDeFormationService()->historise($parcours);
+        return $this->redirect()->toRoute('parcours-de-formation', [], ['fragment' => $this->getFragment($parcours)], true);
+    }
+
+    public function restaurerAction()
+    {
+        $parcours = $this->getParcoursDeFormationService()->getRequestedParcoursDeFormation($this);
+        $this->getParcoursDeFormationService()->restore($parcours);
+        return $this->redirect()->toRoute('parcours-de-formation', [], ['fragment' => $this->getFragment($parcours)], true);
+    }
+
+    public function detruireAction()
+    {
+        /** @var ParcoursDeFormation $parcours */
+        $parcours = $this->getParcoursDeFormationService()->getRequestedParcoursDeFormation($this);
+
+        /** @var Request $request */
+        $request = $this->getRequest();
+        if ($request->isPost()) {
+            $data = $request->getPost();
+            if ($data["reponse"] === "oui") $this->getParcoursDeFormationService()->delete($parcours);
+            exit();
+        }
+
+        $vm = new ViewModel();
+        if ($parcours !== null) {
+            $vm->setTemplate('application/default/confirmation');
+            $vm->setVariables([
+                'title' => "Suppression du parcours de formation [" . $parcours->getLibelle() . "]",
+                'text' => "La suppression est définitive êtes-vous sûr&middot;e de vouloir continuer ?",
+                'action' => $this->url()->fromRoute('parcours-de-formation/detruire', ["parcours-de-formation" => $parcours->getId()], [], true),
+            ]);
+        }
+        return $vm;
+    }
 }
