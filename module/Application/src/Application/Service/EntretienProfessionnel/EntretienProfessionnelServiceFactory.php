c46cf01ba6477d757fa873716936d5dacee5c22e
metivier <jean-philippe.metivier@unicaen.fr>
2020-09-16 14:54:15 +0200

    Oublie d'injection corrigée

--- module/Application/src/Application/Service/EntretienProfessionnel/EntretienProfessionnelServiceFactory.php
+++ module/Application/src/Application/Service/EntretienProfessionnel/EntretienProfessionnelServiceFactory.php
@@ -2,6 +2,8 @@
 
 namespace Application\Service\EntretienProfessionnel;
 
+use Application\Service\Configuration\ConfigurationService;
+use Autoform\Service\Formulaire\FormulaireInstanceService;
 use Doctrine\ORM\EntityManager;
 use Interop\Container\ContainerInterface;
 use UnicaenUtilisateur\Service\User\UserService;
@@ -12,10 +14,14 @@ class EntretienProfessionnelServiceFactory {
     {
         /**
          * @var EntityManager $entityManager
+         * @var ConfigurationService $configurationService
          * @var UserService $userService
+         * @var FormulaireInstanceService $formulaireInstanceService
          */
         $entityManager = $container->get('doctrine.entitymanager.orm_default');
         $userService = $container->get(UserService::class);
+        $configurationService = $container->get(ConfigurationService::class);
+        $formulaireInstanceService = $container->get(FormulaireInstanceService::class);
 
         /**
          * @var EntretienProfessionnelService $service
@@ -23,6 +29,8 @@ class EntretienProfessionnelServiceFactory {
         $service = new EntretienProfessionnelService();
         $service->setEntityManager($entityManager);
         $service->setUserService($userService);
+        $service->setConfigurationService($configurationService);
+        $service->setFormulaireInstanceService($formulaireInstanceService);
         return $service;
     }
 }
