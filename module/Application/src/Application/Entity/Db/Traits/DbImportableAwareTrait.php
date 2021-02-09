56641b6c61a82a94ff91106df393498f26c43d72
Jean-Philippe Metivier <jean-philippe.metivier@unicaen.fr>
2021-02-09 11:23:50 +0100

    ajout du trait oublié

--- module/Application/src/Application/Entity/Db/Traits/DbImportableAwareTrait.php
+++ module/Application/src/Application/Entity/Db/Traits/DbImportableAwareTrait.php
@@ -2,4 +2,20 @@
 
 namespace Application\Entity\Db\Traits;
 
-trait ImportableAwareTrait
+use DateTime;
+
+trait DbImportableAwareTrait {
+
+    /** @var DateTime */
+    private $created_on;
+    /** @var DateTime */
+    private $updated_on;
+    /** @var DateTime */
+    private $deleted_on;
+
+    public function isDeleted(DateTime $date = null) : bool
+    {
+        if ($date === null) return ($this->deleted_on !== null);
+        return $this->deleted_on < $date;
+    }
+}
