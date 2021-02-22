e4faf3d30354143e26af6b91e95c002ba2c6f97b
Jean-Philippe Metivier <jean-philippe.metivier@unicaen.fr>
2021-02-22 17:49:46 +0100

    deplacement bouton selection activite dans l'edition des fiche de poste

--- module/Application/src/Application/View/Helper/FicheMetierViewHelper.php
+++ module/Application/src/Application/View/Helper/FicheMetierViewHelper.php
@@ -4,6 +4,7 @@ namespace Application\View\Helper;
 
     use Application\Entity\Db\FicheMetier;
     use Application\Entity\Db\FichePoste;
+    use Application\Entity\Db\FicheTypeExterne;
     use Application\View\Renderer\PhpRenderer;
     use Zend\View\Helper\AbstractHelper;
     use Zend\View\Helper\Partial;
@@ -14,15 +15,16 @@ class FicheMetierViewHelper extends AbstractHelper
     /**
      * @param FicheMetier $fichemetier
      * @param FichePoste $ficheposte
+     * @param FicheTypeExterne $ficheexterne
      * @param array $options
      * @return string|Partial
      */
-    public function __invoke($fichemetier, $ficheposte = null, $options = [])
+    public function __invoke($fichemetier, $ficheposte = null, $ficheexterne = null, $options = [])
     {
         /** @var PhpRenderer $view */
         $view = $this->getView();
         $view->resolver()->attach(new TemplatePathStack(['script_paths' => [__DIR__ . "/partial"]]));
 
-        return $view->partial('fiche-metier', ['fichemetier' => $fichemetier, 'ficheposte' => $ficheposte, 'options' => $options]);
+        return $view->partial('fiche-metier', ['fichemetier' => $fichemetier, 'ficheposte' => $ficheposte, 'ficheexterne' => $ficheexterne, 'options' => $options]);
     }
 }
