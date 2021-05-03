0e28163ef532cffb126ecd5c58c150f8fcc32593
Jean-Philippe Metivier <jean-philippe.metivier@unicaen.fr>
2021-05-03 10:33:53 +0200

    Fixing problème de date

--- module/Formation/src/Formation/Controller/FormationConsoleController.php
+++ module/Formation/src/Formation/Controller/FormationConsoleController.php
@@ -27,6 +27,9 @@ class FormationConsoleController extends AbstractActionController {
      */
     public function notifierConvocationAction()
     {
+        echo (new DateTime())->format('d/m/y à H:i:s') . "\n";
+        echo "Notifier Convocation" . "\n";
+
         $delai = $this->getParametreService()->getParametreByCode('FORMATION', 'INTERVAL_CONVOCATION');
         if ($delai === null) throw new Exception("Le parametre FORMATION\INTERVAL_CONVOCATION n'est pas déclaré ! ");
         if ($delai->getValeur() == "")       throw new Exception("Le parametre FORMATION\INTERVAL_CONVOCATION n'a pas de valeur ! ");
@@ -35,9 +38,9 @@ class FormationConsoleController extends AbstractActionController {
         $now->add(new DateInterval('P'.$delai->getValeur().'D'));
         $sessions = $this->getFormationInstanceService()->getFormationsInstancesByEtat(FormationInstance::ETAT_INSCRIPTION_FERMEE);
         foreach ($sessions as $session) {
-            $nb = 0;
             $debut = DateTime::createFromFormat('d/m/Y',$session->getDebut());
-            if ($debut <= $delai) {
+            if ($debut <= $now) {
+                $nb = 0;
                 foreach ($session->getListePrincipale() as $inscrit) {
                     $mail = $this->getMailingService()->sendMailType('FORMATION_CONVOCATION', ['formation' => $session->getFormation(), 'instance' => $session, 'agent' => $inscrit->getAgent(), 'mailing' => $inscrit->getAgent()->getEmail()]);
                     $mail->setAttachementType(FormationInstance::class);
@@ -45,12 +48,13 @@ class FormationConsoleController extends AbstractActionController {
                     $this->getMailingService()->update($mail);
                     $nb++;
                 }
+                $session->setEtat($this->getEtatService()->getEtatByCode(FormationInstance::ETAT_FORMATION_CONVOCATION));
+                $this->getFormationInstanceService()->update($session);
+                echo (new DateTime())->format('d/m/y à H:i:s') . "\n";
+                echo "Envoi des convocations effectué pour la session de formation " . $session->getFormation()->getLibelle() . " - " . $session->getId() . "\n";
+                echo $nb . " convocations envoyées.\n";
             }
-            $session->setEtat($this->getEtatService()->getEtatByCode(FormationInstance::ETAT_FORMATION_CONVOCATION));
-            $this->getFormationInstanceService()->update($session);
-            echo (new DateTime())->format('d/m/y à H:i:s') . "\n";
-            echo "Envoi des convocations effectué pour la session de formation " . $session->getFormation()->getLibelle() . " - " . $session->getId() . "\n";
-            echo $nb . " convocations envoyées.\n";
+
         }
     }
 }
