ef7920384ace4410a6b53343aade428662f7ee28
Jean-Philippe Metivier <jean-philippe.metivier@unicaen.fr>
2021-10-11 16:35:20 +0200

    Ajustement de la génération des pdfs

--- module/EntretienProfessionnel/src/EntretienProfessionnel/Controller/EntretienProfessionnelControllerFactory.php
+++ module/EntretienProfessionnel/src/EntretienProfessionnel/Controller/EntretienProfessionnelControllerFactory.php
@@ -20,7 +20,6 @@ use Mailing\Service\Mailing\MailingService;
 use UnicaenEtat\Service\Etat\EtatService;
 use UnicaenEtat\Service\EtatType\EtatTypeService;
 use UnicaenRenderer\Service\Contenu\ContenuService;
-use UnicaenRenderer\Service\Template\TemplateService;
 use UnicaenUtilisateur\Service\User\UserService;
 use UnicaenValidation\Service\ValidationInstance\ValidationInstanceService;
 use UnicaenValidation\Service\ValidationType\ValidationTypeService;
