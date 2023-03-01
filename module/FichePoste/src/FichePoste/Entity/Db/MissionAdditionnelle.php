d8d01a4fd69ad491420d3794609cff96a643eb9f
Jean-Philippe Metivier <jean-philippe.metivier@unicaen.fr>
2023-03-01 18:43:18 +0100

    Progress

--- module/FichePoste/src/FichePoste/Entity/Db/MissionAdditionnelle.php
+++ module/FichePoste/src/FichePoste/Entity/Db/MissionAdditionnelle.php
@@ -1,18 +1,17 @@
 <?php
 
-namespace Application\Entity\Db;
 
 use UnicaenUtilisateur\Entity\Db\HistoriqueAwareInterface;
 use UnicaenUtilisateur\Entity\Db\HistoriqueAwareTrait;
 
-class SpecificiteActivite implements HistoriqueAwareInterface {
+class MissionAdditionnelle implements HistoriqueAwareInterface {
     use HistoriqueAwareTrait;
 
     /** @var int */
     private $id;
-    /** @var SpecificitePoste */
+    /** @var \Application\Entity\Db\SpecificitePoste */
     private $specificite;
-    /** @var Activite */
+    /** @var \Application\Entity\Db\Activite */
     private $activite;
     /** @var string|null */
     private $retrait;
@@ -28,36 +27,36 @@ class SpecificiteActivite implements HistoriqueAwareInterface {
     }
 
     /**
-     * @return SpecificitePoste|null
+     * @return \Application\Entity\Db\SpecificitePoste|null
      */
-    public function getSpecificite(): ?SpecificitePoste
+    public function getSpecificite(): ?\Application\Entity\Db\SpecificitePoste
     {
         return $this->specificite;
     }
 
     /**
-     * @param SpecificitePoste $specificite
-     * @return SpecificiteActivite
+     * @param \Application\Entity\Db\SpecificitePoste $specificite
+     * @return \Application\Entity\Db\SpecificiteActivite
      */
-    public function setSpecificite(SpecificitePoste $specificite): SpecificiteActivite
+    public function setSpecificite(\Application\Entity\Db\SpecificitePoste $specificite): MissionAdditionnelle
     {
         $this->specificite = $specificite;
         return $this;
     }
 
     /**
-     * @return Activite|null
+     * @return \Application\Entity\Db\Activite|null
      */
-    public function getActivite(): ?Activite
+    public function getActivite(): ?\Application\Entity\Db\Activite
     {
         return $this->activite;
     }
 
     /**
-     * @param Activite $activite
-     * @return SpecificiteActivite
+     * @param \Application\Entity\Db\Activite $activite
+     * @return \Application\Entity\Db\SpecificiteActivite
      */
-    public function setActivite(Activite $activite): SpecificiteActivite
+    public function setActivite(\Application\Entity\Db\Activite $activite): MissionAdditionnelle
     {
         $this->activite = $activite;
         return $this;
@@ -73,9 +72,9 @@ class SpecificiteActivite implements HistoriqueAwareInterface {
 
     /**
      * @param string|null $retrait
-     * @return SpecificiteActivite
+     * @return \Application\Entity\Db\SpecificiteActivite
      */
-    public function setRetrait(?string $retrait): SpecificiteActivite
+    public function setRetrait(?string $retrait): MissionAdditionnelle
     {
         $this->retrait = $retrait;
         return $this;
@@ -91,9 +90,9 @@ class SpecificiteActivite implements HistoriqueAwareInterface {
 
     /**
      * @param string|null $description
-     * @return SpecificiteActivite
+     * @return \Application\Entity\Db\SpecificiteActivite
      */
-    public function setDescription(?string $description): SpecificiteActivite
+    public function setDescription(?string $description): MissionAdditionnelle
     {
         $this->description = $description;
         return $this;
