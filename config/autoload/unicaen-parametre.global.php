c8ec60ec1ebe3573daf9a06c51dc42e36ad19903
Jean-Philippe Metivier <jean-philippe.metivier@unicaen.fr>
2023-03-24 15:24:27 +0100

    Remise du menu des parametre dans l'administration général

---
+++ config/autoload/unicaen-parametre.global.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace UnicaenParametre;
+
+use Carriere\Controller\CategorieController;
+use UnicaenPrivilege\Guard\PrivilegeController;
+
+return [
+
+    'navigation' => [
+        'default' => [
+            'home' => [
+                'pages' => [
+                    'administration' => [
+                        'pages' => [
+                            'parametre' => [
+                                'label' => 'Paramètres',
+                                'route' => 'parametre/index',
+                                'resource' => PrivilegeController::getResourceId(CategorieController::class, 'index'),
+                                'order' => 7020,
+                                'pages' => [],
+                                'icon' => 'fas fa-angle-right',
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+        ],
+    ],
+];
