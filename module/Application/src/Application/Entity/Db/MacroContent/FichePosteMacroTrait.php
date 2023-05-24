7d16ca71b5e63a7b4730a20abe13dcc4448e4db2
Jean-Philippe Metivier <jean-philippe.metivier@unicaen.fr>
2023-05-24 10:34:47 +0200

    [Fix] : Propagation de la modification des Description retirée à la macro servant pour les versions PDF des fiches de poste

--- module/Application/src/Application/Entity/Db/MacroContent/FichePosteMacroTrait.php
+++ module/Application/src/Application/Entity/Db/MacroContent/FichePosteMacroTrait.php
@@ -281,7 +281,7 @@ trait FichePosteMacroTrait {
     {
         /** @var FichePoste $ficheposte */
         $ficheposte = $this;
-        $descriptionsRetirees = array_map(function ($a) { return $a->getDescription()->getId(); }, $ficheposte->getDescriptionsRetirees());
+        $descriptionsRetirees = array_map(function (FicheposteActiviteDescriptionRetiree $a) { return $a->getActivite()->getId(); }, $ficheposte->getDescriptionsRetirees());
 
         $texte = "";
         foreach ($ficheposte->getFichesMetiers() as $ficheTypeExterne) {
