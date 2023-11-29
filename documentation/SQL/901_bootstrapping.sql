ed442645ae33087b5895af4530e471a9538f5eda
Jean-Philippe Metivier <jean-philippe.metivier@unicaen.fr>
2023-11-29 18:14:48 +0100

    Formation et bootstrapping amélioré

--- documentation/SQL/901_bootstrapping.sql
+++ documentation/SQL/901_bootstrapping.sql
@@ -9,6 +9,16 @@ FROM d
 JOIN unicaen_utilisateur_role cp ON cp.role_id = 'Administrateur·trice technique'
 ;
 
+-- NOMMER UN·E ADMINSTRATEUR·TRICE TECHNIQUE
+INSERT INTO unicaen_utilisateur_user (username, display_name, email, password)
+VALUES ('metivier', 'Jean-Philippe METIVIER', 'jean-philippe.metivier@unicaen.fr', 'ldap');
+
+INSERT INTO unicaen_utilisateur_role_linker (user_id, role_id)
+SELECT u.id AS user_id, r.id AS role_id
+FROM unicaen_utilisateur_user u
+         JOIN unicaen_utilisateur_role r ON true
+WHERE u.username='metivier' AND r.role_id='Administrateur·trice technique';
+
 -- VALEUR DE PARAMETRE POUR AIDER LA VERIFICATION DE L'INSTALLATION
 update unicaen_parametre_parametre set valeur='/var/www/html' where code = 'INSTALL_PATH';
 
