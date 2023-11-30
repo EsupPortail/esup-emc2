b5a08e5feaae3e3060ffffc13e7f9e809531585f
Jean-Philippe Metivier <jean-philippe.metivier@unicaen.fr>
2023-11-30 11:59:13 +0100

    Nettoyage de l'ancienne doc

--- documentation/998-INSERT-EMC2-AFFECTATION_PRIV.sql
+++
@@ -1,8 +0,0 @@
-TRUNCATE TABLE unicaen_privilege_privilege_role_linker;
-INSERT INTO unicaen_privilege_privilege_role_linker (privilege_id, role_id)
-WITH d(privilege_id) AS (
-    SELECT id FROM unicaen_privilege_privilege
-)
-SELECT d.privilege_id, cp.id
-FROM d
-JOIN unicaen_utilisateur_role cp ON cp.role_id = 'Administrateur·trice technique';
