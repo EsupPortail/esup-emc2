84499424bbd02ec305cc88053c117bbe1c7b44b9
Jean-Philippe Metivier <jean-philippe.metivier@unicaen.fr>
2024-01-10 16:34:09 +0100

    Montée en version

--- module/Application/src/Application/Controller/VerificationController.php
+++ module/Application/src/Application/Controller/VerificationController.php
@@ -28,7 +28,7 @@ class VerificationController extends AbstractActionController {
 
     public function indexAction() : ViewModel
     {
-        $modules = ['Application', 'Carriere', 'Element', 'EntretienProfessionnel', 'FichePoste', 'Formation', 'Metier', 'Structure'];
+        $modules = ['Application', 'Carriere', 'Element', 'EntretienProfessionnel', 'FichePoste', 'Formation', 'Metier', 'MissionSpecifique', 'Structure'];
         $bibliotheques = ['indicateur', 'etat', 'validation', 'renderer','autoform', 'evenement', 'mail', 'parametre', 'authentification', 'utilisateur', 'privilege'];
 
         return new ViewModel([
