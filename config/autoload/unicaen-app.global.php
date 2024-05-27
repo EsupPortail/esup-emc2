29612b2de9fe4e5d3cf868c21b76bb0434709eec
Jean-Philippe Metivier <jean-philippe.metivier@unicaen.fr>
2024-05-27 14:14:21 +0200

    Version et verif

--- config/autoload/unicaen-app.global.php
+++ config/autoload/unicaen-app.global.php
@@ -34,8 +34,8 @@ return [
         'app_infos' => [
             'nom'     => "EMC2",
             'desc'    => "Emploi Mobilité Carrière Compétences",
-            'version' => "4.4.9-dev",
-            'date'    => "XX/XX/2024",
+            'version' => "4.4.9",
+            'date'    => "27/05/2024",
 
 //            'liens' => [
 //                'COMUE' => [
