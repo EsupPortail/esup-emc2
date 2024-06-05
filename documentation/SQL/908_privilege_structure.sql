4d52438884649bca40bfade5c4a6997eb7355649
Jean-Philippe Metivier <jean-philippe.metivier@unicaen.fr>
2024-06-05 14:03:22 +0200

    WIP

--- documentation/SQL/908_privilege_structure.sql
+++ documentation/SQL/908_privilege_structure.sql
@@ -1,16 +1,16 @@
-select
-    concat(
-            'insert into unicaen_privilege_privilege_role_linker (role_id, privilege_id) VALUES (',
-            concat('(select id from unicaen_utilisateur_role urr where urr.role_id = ''', uur.role_id, ''')'),
-            ',',
-            concat('(select id from unicaen_privilege_privilege upp where upp.code = ''', upp.code, ''' and upp.categorie_id = (select id from unicaen_privilege_categorie where code =''', upc.code , '''))'),
-            ');'
-    )
-from unicaen_privilege_privilege_role_linker l
-         join unicaen_privilege_privilege upp on l.privilege_id = upp.id
-         join unicaen_privilege_categorie upc on upp.categorie_id = upc.id
-         join unicaen_utilisateur_role uur on l.role_id = uur.id
-where upc.namespace = 'Structure\Provider\Privilege';
+-- select
+--     concat(
+--             'insert into unicaen_privilege_privilege_role_linker (role_id, privilege_id) VALUES (',
+--             concat('(select id from unicaen_utilisateur_role urr where urr.role_id = ''', uur.role_id, ''')'),
+--             ',',
+--             concat('(select id from unicaen_privilege_privilege upp where upp.code = ''', upp.code, ''' and upp.categorie_id = (select id from unicaen_privilege_categorie where code =''', upc.code , '''))'),
+--             ');'
+--     )
+-- from unicaen_privilege_privilege_role_linker l
+--          join unicaen_privilege_privilege upp on l.privilege_id = upp.id
+--          join unicaen_privilege_categorie upc on upp.categorie_id = upc.id
+--          join unicaen_utilisateur_role uur on l.role_id = uur.id
+-- where upc.namespace = 'Structure\Provider\Privilege';
 
 -- MODULE STRUCTURE
 insert into unicaen_privilege_privilege_role_linker (role_id, privilege_id) VALUES ((select id from unicaen_utilisateur_role urr where urr.role_id = 'Administrateur·trice technique'),(select id from unicaen_privilege_privilege upp where upp.code = 'structure_gestionnaire' and upp.categorie_id = (select id from unicaen_privilege_categorie where code ='structure')));
