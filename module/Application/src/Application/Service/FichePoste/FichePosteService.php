3c8d5e841f2d6c98a2b1ec25d1bb1d25883eebc8
Jean-Philippe Metivier <jean-philippe.metivier@unicaen.fr>
2025-10-16 08:12:00 +0200

    Git cleaning

--- module/Application/src/Application/Service/FichePoste/FichePosteService.php
+++ module/Application/src/Application/Service/FichePoste/FichePosteService.php
@@ -3,7 +3,6 @@
 namespace Application\Service\FichePoste;
 
 use Application\Entity\Db\Agent;
-use Application\Entity\Db\DomaineRepartition;
 use Application\Entity\Db\FichePoste;
 use Application\Entity\Db\FicheposteApplicationRetiree;
 use Application\Entity\Db\FicheTypeExterne;
