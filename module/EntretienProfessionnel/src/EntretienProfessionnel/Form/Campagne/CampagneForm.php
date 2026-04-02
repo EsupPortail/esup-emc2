4ca76c4f74954a9c10a93474a781b92cae9a38f7
Jean-Philippe METIVIER <jean-philippe.metivier@unicaen.fr>
2026-04-02 11:36:08 +0200

    ...

--- module/EntretienProfessionnel/src/EntretienProfessionnel/Form/Campagne/CampagneForm.php
+++ module/EntretienProfessionnel/src/EntretienProfessionnel/Form/Campagne/CampagneForm.php
@@ -97,7 +97,7 @@ class CampagneForm extends Form
             'name' => 'date_en_poste',
             'type' => Date::class,
             'options' => [
-                'label' => "Date à laquelle sera considérée la situation de l'agent pour déterminer le caractère obligatoire des entretiens <span class='icon icon-information' title=\"Les affectations, statuts et grades sont examinés à cette date.\"></span> <span class='icon icon-obligatoire' title='Champ obligatoire'></span>:",
+                'label' => "Date à laquelle sera considérée la situation de l'agent pour déterminer le caractère obligatoire des entretiens :",
                 'label_options' => [ 'disable_html_escape' => true, ],
                 'label_attributes' => [
                     'class' => 'control-label',
