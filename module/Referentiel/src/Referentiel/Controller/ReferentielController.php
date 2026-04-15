ad5bb80d88a095ecc14d610ae7a8b022f084a861
Jean-Philippe METIVIER <jean-philippe.metivier@unicaen.fr>
2026-04-15 11:01:27 +0200

    ...

--- module/Referentiel/src/Referentiel/Controller/ReferentielController.php
+++ module/Referentiel/src/Referentiel/Controller/ReferentielController.php
@@ -2,6 +2,18 @@
 
 namespace Referentiel\Controller;
 
+use Element\Controller\CompetenceController;
+use Element\Provider\Privilege\CompetencePrivileges;
+use Element\Service\Competence\CompetenceServiceAwareTrait;
+use FicheMetier\Controller\ActiviteController;
+use FicheMetier\Controller\FicheMetierController;
+use FicheMetier\Controller\MissionPrincipaleController;
+use FicheMetier\Provider\Privilege\ActivitePrivileges;
+use FicheMetier\Provider\Privilege\FicheMetierPrivileges;
+use FicheMetier\Provider\Privilege\MissionPrincipalePrivileges;
+use FicheMetier\Service\Activite\ActiviteServiceAwareTrait;
+use FicheMetier\Service\FicheMetier\FicheMetierServiceAwareTrait;
+use FicheMetier\Service\MissionPrincipale\MissionPrincipaleServiceAwareTrait;
 use Laminas\Http\Request;
 use Laminas\Http\Response;
 use Laminas\Mvc\Controller\AbstractActionController;
@@ -9,10 +21,18 @@ use Laminas\View\Model\ViewModel;
 use Referentiel\Entity\Db\Referentiel;
 use Referentiel\Form\Referentiel\ReferentielFormAwareTrait;
 use Referentiel\Service\Referentiel\ReferentielServiceAwareTrait;
+use UnicaenPrivilege\Service\Privilege\PrivilegeServiceAwareTrait;
+use UnicaenUtilisateur\Service\User\UserServiceAwareTrait;
 
 class ReferentielController extends AbstractActionController
 {
+    use ActiviteServiceAwareTrait;
+    use CompetenceServiceAwareTrait;
+    use FicheMetierServiceAwareTrait;
+    use MissionPrincipaleServiceAwareTrait;
+    use PrivilegeServiceAwareTrait;
     use ReferentielServiceAwareTrait;
+    use UserServiceAwareTrait;
     use ReferentielFormAwareTrait;
 
     public function indexAction(): ViewModel
@@ -115,15 +135,75 @@ class ReferentielController extends AbstractActionController
         $request = $this->getRequest();
         if ($request->isPost()) {
             $data = $request->getPost();
-            if ($data["reponse"] === "oui") $this->getReferentielService()->delete($referentiel);
+            if ($data["reponse"] === "oui") {
+                foreach ($referentiel->getActivites() as $activite) $this->getActiviteService()->delete($activite);
+                foreach ($referentiel->getMissions() as $mission) $this->getMissionPrincipaleService()->delete($mission);
+                foreach ($referentiel->getCompetences() as $competence) $this->getCompetenceService()->delete($competence);
+                foreach ($referentiel->getFichesMetiers() as $fiche) $this->getFicheMetierService()->delete($fiche);
+                $this->getReferentielService()->delete($referentiel);
+            }
             exit();
         }
 
         $vm = new ViewModel();
         if ($referentiel !== null) {
+            $role = $this->getUserService()->getConnectedRole();
+            $warnings = "";
+
+            $nbActivite = count($referentiel->getActivites());
+            if ($nbActivite > 0) {
+                $warnings .= "<li>".$nbActivite . " activités ";
+                if ($this->getPrivilegeService()->checkPrivilege(ActivitePrivileges::ACTIVITE_INDEX, $role)) {
+                    /** @see ActiviteController::indexAction() **/
+                    $urlActivite = $this->url()->fromRoute('activite', [], ['query' => ['referentiel' => $referentiel->getId()]], true);
+                    $warnings .= "<a href='".$urlActivite."' class='action secondary' target='_blank'> <span class='icon icon-voir'  style='color: gray;'></span> Accéder aux activités </a>";
+                }
+                $warnings .= "</li>";
+            }
+            $nbMission = count($referentiel->getMissions());
+            if ($nbMission > 0) {
+                $warnings .= "<li>".$nbMission . " missions ";
+                if ($this->getPrivilegeService()->checkPrivilege(MissionPrincipalePrivileges::MISSIONPRINCIPALE_INDEX, $role)) {
+                    /** @see MissionPrincipaleController::indexAction() */
+                    $urlMission = $this->url()->fromRoute('mission-principale', [], ['query' => ['referentiel' => $referentiel->getId()]], true);
+                    $warnings .= "<a href='".$urlMission."' class='action secondary' target='_blank'> <span class='icon icon-voir'  style='color: gray;'></span> Accéder aux missions </a>";
+                }
+                $warnings .= "</li>";
+            }
+
+            $nbCompetence = count($referentiel->getCompetences());
+            if ($nbCompetence > 0) {
+                $warnings .= "<li>".$nbCompetence . " compétences ";
+                if ($this->getPrivilegeService()->checkPrivilege(CompetencePrivileges::COMPETENCE_INDEX, $role)) {
+                    /** @see CompetenceController::indexAction() */
+                    $urlCompetence = $this->url()->fromRoute('element/competence/listing', [], ['query' => ['referentiel' => $referentiel->getId()]], true);
+                    $warnings .= "<a href='".$urlCompetence."' class='action secondary' target='_blank'> <span class='icon icon-voir'  style='color: gray;'></span> Accéder aux compétences </a>";
+                }
+                $warnings .= "</li>";
+            }
+
+            $nbFiche = count($referentiel->getFichesMetiers());
+            if ($nbFiche > 0) {
+                $warnings .= "<li>".$nbFiche . " fiches métiers ";
+                if ($this->getPrivilegeService()->checkPrivilege(FicheMetierPrivileges::FICHEMETIER_INDEX, $role)) {
+                    /** @see FicheMetierController::indexAction() */
+                    $urlCompetence = $this->url()->fromRoute('fiche-metier', [], ['query' => ['referentiel' => $referentiel->getId()]], true);
+                    $warnings .= "<a href='".$urlCompetence."' class='action secondary' target='_blank'> <span class='icon icon-voir' style='color: gray;'></span> Accéder aux fiches métiers </a>";
+                }
+                $warnings .= "</li>";
+            }
+            if ($warnings !== '') {
+                $texteWarning  = "<strong class='text-danger'>ATTENTION :</strong> la suppression du référentiel entraîne la suppression des ressources attachées.<br>";
+                $texteWarning .= "Ressources attachées au référentiel :<ul>".$warnings."</ul>";
+            }
+
+
+            else $texteWarning = null;
+
             $vm->setTemplate('default/confirmation');
             $vm->setVariables([
                 'title' => "Suppression du référentiel [".$referentiel->getLibelleCourt()."]",
+                'warning' => $texteWarning,
                 'text' => "La suppression est définitive êtes-vous sûr&middot;e de vouloir continuer ?",
                 'action' => $this->url()->fromRoute('referentiel/supprimer', ["referentiel" => $referentiel->getId()], [], true),
             ]);
--- module/Referentiel/src/Referentiel/Controller/ReferentielControllerFactory.php
+++ module/Referentiel/src/Referentiel/Controller/ReferentielControllerFactory.php
@@ -2,11 +2,17 @@
 
 namespace Referentiel\Controller;
 
+use Element\Service\Competence\CompetenceService;
+use FicheMetier\Service\Activite\ActiviteService;
+use FicheMetier\Service\FicheMetier\FicheMetierService;
+use FicheMetier\Service\MissionPrincipale\MissionPrincipaleService;
 use Psr\Container\ContainerExceptionInterface;
 use Psr\Container\ContainerInterface;
 use Psr\Container\NotFoundExceptionInterface;
 use Referentiel\Form\Referentiel\ReferentielForm;
 use Referentiel\Service\Referentiel\ReferentielService;
+use UnicaenPrivilege\Service\Privilege\PrivilegeService;
+use UnicaenUtilisateur\Service\User\UserService;
 
 class ReferentielControllerFactory
 {
@@ -18,14 +24,32 @@ class ReferentielControllerFactory
     public function __invoke(ContainerInterface $container): ReferentielController
     {
         /**
+         * @var ActiviteService $activiteService
+         * @var CompetenceService $competenceService
+         * @var FicheMetierService $ficheMetierService
+         * @var MissionPrincipaleService $missionPrincipaleService
+         * @var PrivilegeService $privilegeService
          * @var ReferentielService $referentielService
+         * @var UserService $userService
          * @var ReferentielForm $referentielForm
          */
+        $activiteService = $container->get(ActiviteService::class);
+        $competenceService = $container->get(CompetenceService::class);
+        $ficheMetierService = $container->get(FicheMetierService::class);
+        $missionPrincipaleService = $container->get(MissionPrincipaleService::class);
+        $privilegeService = $container->get(PrivilegeService::class);
         $referentielService = $container->get(ReferentielService::class);
+        $userService = $container->get(UserService::class);
         $referentielForm = $container->get('FormElementManager')->get(ReferentielForm::class);
 
         $controller = new ReferentielController();
+        $controller->setActiviteService($activiteService);
+        $controller->setCompetenceService($competenceService);
+        $controller->setFicheMetierService($ficheMetierService);
+        $controller->setMissionPrincipaleService($missionPrincipaleService);
+        $controller->setPrivilegeService($privilegeService);
         $controller->setReferentielService($referentielService);
+        $controller->setUserService($userService);
         $controller->setReferentielForm($referentielForm);
         return $controller;
     }
