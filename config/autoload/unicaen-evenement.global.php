aabaeb0ef99e06b0689a0a24e31d2ea34a70c578
Jean-Philippe METIVIER <jean-philippe.metivier@unicaen.fr>
2026-04-20 14:34:08 +0200

    Ajustement unicaen-evenement

--- config/autoload/unicaen-evenement.global.php
+++ config/autoload/unicaen-evenement.global.php
@@ -1,29 +1,43 @@
 <?php
 
-use Application\Event\RgpdRenderer\RgpdRendererEvenement;
-use Application\Provider\EvenementProvider;
-use Application\Provider\EvenementProvider as ApplicationEvenementProvider;
-use EntretienProfessionnel\Provider\Event\EvenementProvider as EntretienProfessionnelEvenementProvider;
-use EntretienProfessionnel\Service\Evenement\RappelCampagneAvancementAutoriteService;
-use EntretienProfessionnel\Service\Evenement\RappelCampagneAvancementSuperieurService;
-use EntretienProfessionnel\Service\Evenement\RappelEntretienProfessionnelService;
-use EntretienProfessionnel\Service\Evenement\RappelPasObservationService;
+use UnicaenEvenement\Controller\IndexController;
 use UnicaenEvenement\Entity\Db\Type;
 use UnicaenEvenement\Service\EvenementCollection\EvenementCollectionService;
+use UnicaenPrivilege\Guard\PrivilegeController;
+
 
 return [
     'unicaen-evenement' => [
         'service' => [
             // Évènements de base
             Type::COLLECTION => EvenementCollectionService::class,
-
-            EvenementProvider::RGPD_UNICAEN_RENDERER => RgpdRendererEvenement::class,
-
-
-            EntretienProfessionnelEvenementProvider::RAPPEL_CAMPAGNE_AVANCEMENT_AUTORITE => RappelCampagneAvancementAutoriteService::class,
-            EntretienProfessionnelEvenementProvider::RAPPEL_CAMPAGNE_AVANCEMENT_SUPERIEUR => RappelCampagneAvancementSuperieurService::class,
-            EntretienProfessionnelEvenementProvider::RAPPEL_ENTRETIEN_PROFESSIONNEL => RappelEntretienProfessionnelService::class,
-            EntretienProfessionnelEvenementProvider::RAPPEL_PAS_OBSERVATION_ENTRETIEN_PROFESSIONNEL => RappelPasObservationService::class,
         ],
+
+        'icone' => [
+            Type::COLLECTION => 'icon icon-listing',
+        ],
+    ],
+
+    'navigation' => [
+        'default' => [
+            'home' => [
+                'pages' => [
+                    'administration' => [ // >>>>> adaptez la clé selon votre menu Administration, le cas échéant.
+                        'pages' => [
+                            'unicaen-evenement' => [
+                                'label' => 'Événement',
+                                'route' => 'unicaen-evenement',
+                                'resource' => PrivilegeController::getResourceId(IndexController::class, 'index'),
+                                'order' => 11002,
+                                'icon' => 'fas fa-angle-right',
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+        ],
+
+        /** Délai maximal avant l'annulation pour faute de trop grand retard  */
+        'delai-peremption' => 'P7D',
     ],
 ];
