6eb347c733dcb960825e03f7bf2ac22b68382aa8
Jean-Philippe METIVIER <jean-philippe.metivier@unicaen.fr>
2026-05-29 14:23:34 +0200

    Type

--- module/Agent/src/Agent/Provider/Privilege/FichePostePrivileges.php
+++ module/Agent/src/Agent/Provider/Privilege/FichePostePrivileges.php
@@ -4,7 +4,9 @@ namespace Agent\Provider\Privilege;
 
 use UnicaenPrivilege\Provider\Privilege\Privileges;
 
+// Bandage sur jambe de bois !!!
 class FichePostePrivileges extends Privileges
 {
     const FICHEPOSTE_AJOUTER_PDF            = 'ficheposte-ficheposte_ajouter_pdf';
+    const FICHEPOSTE_SUPPRIMER_PDF            = 'ficheposte-ficheposte_supprimer_pdf';
 }
