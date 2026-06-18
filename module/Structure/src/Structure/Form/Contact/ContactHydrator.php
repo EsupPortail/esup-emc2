83c18dbf98558c9acb24537a309031e19f826c37
Jean-Philippe METIVIER <jean-philippe.metivier@unicaen.fr>
2026-06-18 08:48:41 +0200

    Modification contact

--- module/Structure/src/Structure/Form/Contact/ContactHydrator.php
+++ module/Structure/src/Structure/Form/Contact/ContactHydrator.php
@@ -19,10 +19,10 @@ class ContactHydrator implements HydratorInterface
         $data = [
             'type' => $object->getType()?->getId(),
             'service' => $object->getService(),
-            'prenom' => $object->getPrenom(),
-            'nom' => $object->getNom(),
+            'denomination' => $object->getDenomination(),
             'telephone' => $object->getTelephone(),
             'email' => $object->getEmail(),
+            'url' => $object->getUrl(),
             'structures' => $structuresId,
         ];
         return $data;
@@ -33,8 +33,7 @@ class ContactHydrator implements HydratorInterface
         /** @var Contact $object */
         $type = (isset($data['type'])) ? $this->getTypeService()->getType($data['type']) : null;
         $service = (isset($data['service']) AND trim($data['service']) !== "")?trim($data['service']):null;
-        $prenom = (isset($data['prenom']) AND trim($data['prenom']) !== "")?trim($data['prenom']):null;
-        $nom = (isset($data['nom']) AND trim($data['nom']) !== "")?trim($data['nom']):null;
+        $denomination = (isset($data['denomination']) AND trim($data['denomination']) !== "")?trim($data['denomination']):null;
         $telephone = (isset($data['telephone']) AND trim($data['telephone']) !== "")?trim($data['telephone']):null;
         $email = (isset($data['email']) AND trim($data['email']) !== "")?trim($data['email']):null;
 
@@ -60,8 +59,7 @@ class ContactHydrator implements HydratorInterface
 
         $object->setType($type);
         $object->setService($service);
-        $object->setPrenom($prenom);
-        $object->setNom($nom);
+        $object->setDenomination($denomination);
         $object->setTelephone($telephone);
         $object->setEmail($email);
         $object->setTmp($structures);
